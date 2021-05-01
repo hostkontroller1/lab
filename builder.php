e4c0e5a1ca804fdea7c09b719d1b1fb6fb15de1e
hostkontroller1 <83040260+hostkontroller1@users.noreply.github.com>
2021-05-01 17:33:38 +0300

    Delete builder.php

--- builder.php
+++
@@ -1,7 +0,0 @@
-<?php
-$filename = "version";
-$version = trim(shell_exec('git symbolic-ref --short -q HEAD'));
-file_put_contents($filename, $version);
-
-$query = 'composer install';
-$tmp = exec($query);
